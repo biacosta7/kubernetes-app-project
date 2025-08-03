4819ec3ccbd31f6015866a2d42cc56d67cdf0034
biacosta7 <contact.beatrizcosta@gmail.com>
2025-08-03 20:09:05 -0300

    feat: creaate dockerfile, sql and script

---
+++ script.sh
@@ -0,0 +1,8 @@
+echo "Criando as imagens..."
+
+docker build -t beatrizcosta/projeto-backend:1.0 backend/.
+docker build -t beatrizcosta/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens..."
+docker push beatrizcosta/projeto-backend:1.0
+docker push beatrizcosta/projeto-database:1.0
