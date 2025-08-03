ee1c2397756d5fffec8dd0b794778f5939e15f10
biacosta7 <contact.beatrizcosta@gmail.com>
2025-08-03 20:35:47 -0300

    feat: update script with service and deployment application

--- script.sh
+++ script.sh
@@ -6,3 +6,9 @@ docker build -t beatrizcosta/projeto-database:1.0 database/.
 echo "Realizando o push das imagens..."
 docker push beatrizcosta/projeto-backend:1.0
 docker push beatrizcosta/projeto-database:1.0
+
+echo "Criando services no cluster kubernetes..."
+kubctl apply -f ./services.yml
+
+echo "Criando deployments..."
+kubctl apply -f ./deployment.yml
